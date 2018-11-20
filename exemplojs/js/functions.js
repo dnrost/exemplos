3cc9ebea2d248ea33b5a17e178bfcc25fcbd1a6a
dionesfrancisco <dnrost@gmail.com>
2018-11-20 11:09:55 -0200

    add exemples js

---
+++ exemplojs/js/functions.js
@@ -0,0 +1,9 @@
+Date d = new Date();
+var dx = d.getTimezoneOffset();
+
+let c = dx.getMonth();
+
+var par = document.getElementById("_par01").valueOf();
+
+var e = document.getElementById("par03");
+
