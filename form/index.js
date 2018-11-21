0dbd271323612a0b0678b65f578b088f8f37ff98
dionesfrancisco <dnrost@gmail.com>
2018-11-21 09:27:23 -0200

    add

--- form/index.js
+++ form/index.js
@@ -2,3 +2,11 @@ function salva() {
     alert("Animal Salvo");
     alert("Muitos cachorros")
 }
+
+navigator.geolocation.getCurrentPosition(showpos);
+
+    function showpos(position) {
+        lat = position.coords.latitude;
+        lat = position.coords.longitude;
+        alert("Sua posição: " + lat + "," + lon);
+    }
